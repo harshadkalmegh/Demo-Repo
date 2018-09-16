486af8197a43a71d32ae43e849e4d52369e588a9
harshadkalmegh <harshadkalmegh@yahoo.com>
2018-09-16 18:58:10 +0530

    first commit

---
+++ src/main/java/com/qa/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.qa.utils;
+
+public class Utils {
+	
+	public void utilstest(){
+		
+		System.out.println("Utils class");
+	}
+
+}
---
+++ src/test/java/com/qa/test/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void Homet(){
+		
+		System.out.println("Home page test");
+	}
+
+}
